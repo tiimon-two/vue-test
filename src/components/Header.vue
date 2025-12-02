5e8b284757a31dce6f6dee5123685812650d1a5e
Артём <prubv@yandex.ru>
2025-12-02 12:31:31 +0400

    Created a header component, loaded the shopping cart icon, and placed a link to the main page and the shopping cart in the header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="header">
+      <a class="header__logo" href="">
+        <img src="../assets/images/logo.png" width="250" height="100" alt="Astrio logo"/>
+      </a>
+      <a class="header__cart" href="">
+        <img src="../assets/images/cart.svg" width="48" height="48" alt="Cart"/>
+      </a>
+  </header>
+</template>
+
+<style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header__cart {
+    position: relative;
+  }
+
+  .header__cart::after {
+    position: absolute;
+    content: '';
+    width: 15px;
+    height: 15px;
+    right: -10px;
+    top: -10px;
+    border: 2px solid black;
+    border-radius: 50%;
+  }
+</style>
