3d0f7fd45441a0e4b47ce98d6af3971bab58af59
Артём <prubv@yandex.ru>
2025-12-02 16:35:50 +0400

    Add the Catalog component where placed the component with filters

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
   import BaseLayout from './components/BaseLayout.vue';
+import Catalog from './pages/Catalog.vue';
 </script>
 
 <template>
   <BaseLayout>
-    <div></div>
+    <Catalog/>
   </BaseLayout>
 </template>
---
+++ src/pages/Catalog.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Filters from '../components/Filters.vue';
+
+</script>
+
+<template>
+    <section class="catalog">
+        <Filters/>
+        <main>
+          <h2 class="catalog__title">Catalog</h2>
+        </main>
+    </section>
+</template>
+
+<style scoped>
+  .catalog {
+    display: flex;
+  }
+
+  .catalog__title {
+    margin: 4px 0;
+  }
+</style>
