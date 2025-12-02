cce889e745b2f0d5d339a135a77fb7546ab0c3bb
Артём <prubv@yandex.ru>
2025-12-02 12:48:47 +0400

    Created the footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="footer">
+      <p class="footer__text">© 2025 Создано Порубовым А.В.</p>
+    </footer>
+</template>
+
+<style scoped>
+  .footer__text {
+    text-align: right;
+  }
+</style>
