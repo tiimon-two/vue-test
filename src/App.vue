efb7c2a645f2d6685556f42d8de2958c29d3d754
Артём <prubv@yandex.ru>
2025-12-02 15:52:05 +0400

    Added BaseLayout component with header/footer structure. Add global styles for body.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+  import BaseLayout from './components/BaseLayout.vue';
+</script>
+
 <template>
+  <BaseLayout>
     <div></div>
+  </BaseLayout>
 </template>
---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import Header from './Header.vue';
+  import Footer from './Footer.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Header class="header"></Header>
+    <main>
+      <slot></slot>
+    </main>
+    <Footer class="footer"></Footer>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 60px;
+  }
+
+  .header {
+    grid-row: 1 / 2;
+  }
+
+  .footer {
+    grid-row: -1 / -2;
+  }
+</style>
